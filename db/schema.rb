7931999292f08c7138b7583b71b78963157b2067
LebedenkoAndrii <lebedenko1928@gmail.com>
2025-02-15 23:27:22 +0200

    add many_to_many associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_14_123821) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_15_152044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
